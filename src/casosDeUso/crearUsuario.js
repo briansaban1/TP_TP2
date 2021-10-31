e3270f7dfbb7611ac5cb6d805e1d1d944eabaa3d
Brian <info.tododeusa@gmail.com>
2021-10-31 17:55:37 -0300

    se agrega modulo para el envio de mails

--- src/casosDeUso/crearUsuario.js
+++ src/casosDeUso/crearUsuario.js
@@ -1,9 +1,13 @@
 import Usuario, { toDTO } from '../modelos/Usuario.js'
+import enviar_mail from '../servicios/Mailer.js'
 
 async function crearUsuario(daoPersonas, nombre, apellido, usuario, password, email, tipo) {
     const id = Usuario.nextId()
     const persona = new Usuario(id, nombre, apellido, usuario, password, email, tipo)
     await daoPersonas.guardar(toDTO(persona))
+    //se envia mail de registro
+    enviar_mail(email, nombre, apellido)
+    //
     return persona
 }
 
--- src/servicios/Mailer.js
+++ src/servicios/Mailer.js
@@ -1,13 +1,47 @@
-import { credencialesMail } from '../config.js'
+import nodemailer from 'nodemailer'
 
-export default class Mailer {
-    constructor(credenciales) {
-        this.clienteMails = new ClienteMails(credenciales)
-    }
+const MAILUSER = "orty.service@gmail.com"
+const MAILPSSWD = "orty-service-pass"
 
-    async enviar(asunto, destinatario, texto) {
-        await clienteMails.send({
-            body: texto
-        })
-    }
-}
+async function enviar_mail (email, nombre, apellido) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        
+        auth: {
+            user: MAILUSER,
+            pass: MAILPSSWD
+        }
+    });
+    let mail_options = {
+        from: 'orty.service@gmail.com',
+        to: email,
+        subject: 'Bienvenido a ORTY-Service',
+        html: `
+            <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
+                <tr height="200px">  
+                    <td bgcolor="" width="600px">
+                        <h1 style="color: #fff; text-align:center">Bienvenido</h1>
+                        <p  style="color: #fff; text-align:center">
+                            <span style="color: #e84393">${nombre, apellido}</span> 
+                            a la aplicación
+                        </p>
+                    </td>
+                </tr>
+                <tr bgcolor="#fff">
+                    <td style="text-align:center">
+                        <p style="color: #000">Un mundo de servicios a su disposición!</p>
+                    </td>
+                </tr>
+            </table>
+        `
+    };
+   await transporter.sendMail(mail_options, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('El correo se envío correctamente ' + info.response);
+        }
+    });
+};
+
+export default enviar_mail
