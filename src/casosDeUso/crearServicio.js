22f5e968580d52c4436ba4be308a8d181dc6a147
Brian <info.tododeusa@gmail.com>
2021-11-17 00:50:10 -0300

    update

--- src/casosDeUso/crearServicio.js
+++ src/casosDeUso/crearServicio.js
@@ -2,10 +2,10 @@ import Servicios, { toDTO } from '../modelos/Servicios.js'
 import { obtenerCsvServicio } from '../../src/casosDeUso/obtenerCsvServicio.js'
 
 
-async function crearServicio(daoPersonas, titulo, descripcion, telefono, barrio, rubro) {
+async function crearServicio(daoServicios, titulo, descripcion, telefono, barrio, rubro) {
     const id = Servicios.nextId()
     const servicio = new Servicios(id, titulo, descripcion, telefono, barrio, rubro)
-    await daoPersonas.guardar(toDTO(servicio))
+    await daoServicios.guardar(toDTO(servicio))
     
     if(!servicio){
         console.log('servicio no encontrado')
